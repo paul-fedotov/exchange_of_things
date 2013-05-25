51804e8bd9603ffc510a2b779d3e3734c8728d7b
Чимитох <ivanhotimich@yandex.ru>
2013-05-25 13:38:41 +0400

    SQL-запрос на создание БД

---
+++ createDB.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS SWAG;
+CREATE DATABASE SWAG;
+USE SWAG;
+
+DROP TABLE IF EXISTS things;/*таблица вещей*/
+CREATE TABLE things (
+	id INT(5) AUTO_INCREMENT,/*id*/
+	PRIMARY KEY (id),
+	user_id INT(5),/*id пользователя,который залил (из cookie, session берется)*/
+	thingType CHAR(20),/*Тип вещи*/
+	rating INT(5),/*Рейтинг вещи*/
+	countReview int(10),/*Кол-во просмотр*/
+	puctureOne mediumblob,/*Картинка1*/
+	puctureTwo mediumblob,/*Картинка2*/
+	puctureThree mediumblob,/*Картинка3*/
+	cost DECIMAL(10,2),/*цена*/
+	name CHAR(40),/*название*/
+	description CHAR(100)/*Описание*/);
+
+/*	пока не надо
+DROP TABLE IF EXISTS thingsComments;
+CREATE TABLE thingsComments (
+	id INT(5) AUTO_INCREMENT,
+	thing_id INT(5),
+	user_id INT(5),
+	comment CHAR(200));
+*/
+
+DROP TABLE IF EXISTS users;/*таблица пользователей*/
+CREATE TABLE users (
+	user_id INT(5) AUTO_INCREMENT,/*id*/
+	PRIMARY KEY (user_id),
+	name CHAR(40),/*Имя*/
+	login CHAR(20),/*login*/
+	password CHAR(20),/*пароль*/
+	email CHAR(40),/*почта*/
+	rating INT(5),/*Рейтинг*/
+	foto mediumblob,/*фотография*/
+	tel CHAR(20) DEFAULT NULL,/*телефон(не обязательное поле для заполнения)*/
+	city CHAR(20)/*Город*/);
